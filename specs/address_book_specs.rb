5b311ab9be9c743cfa2cfe7e302a4dad55cd07f6
Jamaleh <Jamaleh@hotmail.com>
2015-06-03 17:45:14 +1000

    Assignment 21 completed

--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -34,4 +34,9 @@ RSpec.describe AddressBook do
             expect(new_entry.email).eql? 'augusta.king@lovelace.com'
         end
     end
+    
+    context ".remove_entry" do
+        it "removes entry from the address book" do
+        end
+    end
 end
