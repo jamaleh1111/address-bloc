d58150bad3a8cb43cbfa2619c1a9f0b8359b29fc
Jamaleh <Jamaleh@hotmail.com>
2015-06-05 15:01:46 +1000

    Checkpoint 23 progress

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative "entry.rb"
+require "csv"
 
 class AddressBook
     attr_accessor :entries
@@ -14,11 +15,26 @@ class AddressBook
             if name < entry.name
                 break
             end
-            index += 1
-        end
+               index += 1
+            end
+        
+        def import_from_csv(file_name)
+          csv_text = File.read(file_name)
+          csv = CSV.parse(csv_text, headers: true)
+          csv.each do |row|
+              row_hash = row.to_hash
+              add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+          end
+
+
+        
+        return csv.count
+      end
+                        
         
         @entries.insert(index, Entry.new(name, phone, email))
     end
+
     
     def remove_entry(entry)
         @entries.delete(entry)
--- specs/entry_specs.rb
+++ specs/entry_specs.rb
@@ -16,10 +16,7 @@ RSpec.describe Entry do
         end
     end
     
-<<<<<<< HEAD
 
-=======
->>>>>>> assignment-21
     context ".to_s" do
         it "prints an entry as a string" do
             entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
